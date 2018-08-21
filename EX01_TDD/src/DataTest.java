b44286bcb5d1c181f119744061ccd2654ea4556a
Aylton Almeida <ayltonalmeida2@gmail.com>
2018-08-21 09:26:51 -0300

    Pronto ex1

--- EX01_TDD/src/DataTest.java
+++ EX01_TDD/src/DataTest.java
@@ -20,6 +20,7 @@ class DataTest {
         d.proximoDia();
         assertEquals(2, d.getDia());
     }
+
     @Test
     void testProximoDia_proximoMes() {
         d = new Data(31, 1, 2000);
@@ -27,6 +28,7 @@ class DataTest {
         assertEquals(1, d.getDia());
         assertEquals(2, d.getMes());
     }
+
     @Test
     void testProximoDia_proximoAno() {
         d = new Data(31, 12, 2000);
@@ -39,35 +41,47 @@ class DataTest {
 
     @Test
     void testAdicionaDias() {
-        d = new Data(18, 8, 2018);
-        d.adicionaDias(2);
-        assertEquals(20, d.getDia());
+        d = new Data(1, 1, 2000);
+        d.adicionaDias(1);
+        assertEquals(2, d.getDia(), "Dias");
+        assertEquals(1, d.getMes(), "Meses");
+        assertEquals(2000, d.getAno(), "Anos");
+    }
+
+    @Test
+    void testAdicionaDias_proximoMes() {
+        d = new Data(1, 1, 2000);
+        d.adicionaDias(31);
+        assertEquals(1, d.getDia(), "Dias");
+        assertEquals(2, d.getMes(), "Meses");
+        assertEquals(2000, d.getAno(), "Anos");
+    }
+
+    @Test
+    void testAdicionaDias_proximoAno() {
+        d = new Data(20, 12, 2000);
+        d.adicionaDias(12);
+        assertEquals(1, d.getDia(), "Dias");
+        assertEquals(1, d.getMes(), "Meses");
+        assertEquals(2001, d.getAno(), "Anos");
     }
 
     @Test
     void testDiasNoMes() {
-        d = new Data(18, 8, 2018);
-        int dias = d.diasNoMes(d.getMes());
-        assertNotNull(dias);
-        if (d.getMes() == 2) {
-            if (d.eAnoBisexto()) {
-                assertEquals(dias, 29);
-            } else {
-                assertEquals(dias, 28);
-            }
-        } else {
-            if (d.getMes() == 1 || d.getMes() == 3 || d.getMes() == 5 || d.getMes() == 7 || d.getMes() == 8 || d.getMes() == 10 || d.getMes() == 12) {
-                assertEquals(dias, 31);
-            } else {
-                assertEquals(dias, 30);
-            }
-        }
+        d = new Data(1, 1, 2000);
+        assertEquals(31, d.diasNoMes(), "Teste janeira");
+        d.setMes(4);
+        assertEquals(30, d.diasNoMes(), "Teste abril");
+        d.setMes(2);
+        assertEquals(29, d.diasNoMes(), "Teste fevereiro bisexto");
+        d.setAno(2001);
+        assertEquals(28, d.diasNoMes(), "Teste fevereiro não bisexto");
     }
 
     @Test
-    void testDiaDaSemana(){
-        d = new Data(19, 8, 2018);
-        assertEquals("domingo", d.diaDaSemana());
+    void testDiaDaSemana() {
+        d = new Data(1, 1, 2000);
+        assertEquals("sábado", d.diaDaSemana());
     }
 
 }
